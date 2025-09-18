3f54afb31e45fa8406fbb9481a99b0e83d704464
omarmorad <omorad5362@gmail.com>
2025-09-18 23:02:57 +0300

    refactor(app): remove unused answer widget and simplify main app structure
    removed the Answer widget file and streamlined the main.dart file by:
    - deleting the old question/answer implementation
    - replacing the MyApp widget with a simpler QuizApp widget
    - updating imports to use the new welcome screen
    - removing unused dependencies and code
    - setting up basic app configuration with theme and home screen
    
    this refactors the application structure to use a more modern approach with a dedicated welcome screen instead of the previous inline question/answer components.

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,28 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import 'package:flutter/services.dart';
+import 'screens/welcome_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const QuizApp());
 }
 
-class MyApp extends StatefulWidget {
+class QuizApp extends StatelessWidget {
+  const QuizApp({Key? key}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-  final _questions = const [{
-    'text': 'What\'s your favorite color?',
-    'answers': ['Black', 'Red', 'Green', 'Blue'],
-  },
-  {
-    'text': 'What\'s your favorite animal?',
-    'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-  },
-  {
-    'text': 'What\'s your favorite instructor?',
-    'answers': ['Max', 'Manu', 'Angela', 'Stephen'],
-  },
-  {
-    'text': 'What\'s your favorite food?',
-    'answers': ['Pizza', 'Burger', 'Salad', 'Pasta'],
-  },
-  ];
-
-  void _answerQuestion() {
-    setState(() {
-      if (_questionIndex < _questions.length - 1) {
-        _questionIndex = _questionIndex + 1;
-      } else {
-        _questionIndex = 0; // Reset or handle end of questions
-      }
-    });
-    print('Answer chosen');
-  }
-
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('First Demo App')),
-        body: Column(
-          children: [
-            Question(_questions[_questionIndex]['text'] as String),
-            ...(_questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList(),
-          ],
+      title: 'Flutter & Dart Quiz',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: 'Roboto',
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        appBarTheme: const AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle.light,
         ),
       ),
+      home: const WelcomeScreen(),
     );
   }
 }
