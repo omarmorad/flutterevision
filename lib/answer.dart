0f8d9d104ad8927521aed235a4ede7e4cd5f1cf4
omarmorad <omorad5362@gmail.com>
2025-09-18 17:39:29 +0300

    continue playing around

---
+++ lib/answer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  const Answer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
